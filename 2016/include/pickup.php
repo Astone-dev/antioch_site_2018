0217a24eb6e80b6167a6e03c333bd298b03a8f7b
fujita-PC\fujita <johnfuji4845@gmail.com>
2018-04-15 23:50:02 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -41,7 +41,7 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/1804vision/" target="_blank"><img src="/2016/images/top/1804midoriantioch.jpg"></a>
+<a href="http://astone.tv/events/1804vision/" target="_blank"><img src="/2016/images/top/1610thevision.jpg"></a>
 <p class="block-date">日程：2018年4月16日～22日</p>
 <p class="block-title"><a href="http://astone.tv/events/1804vision/" target="_blank">The Visionの一週間</a></p>
 </div>
