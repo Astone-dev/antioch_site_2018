bcb2fca182f18d54243fe0be345e84504709d5ef
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-04-24 13:24:32 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -63,19 +63,19 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/week1804z/" target="_blank"><img src="/2016/images/top/1804sweetszenkoku.jpg"></a>
-<p class="block-date">日程：2018年4月23日～29日</p>
-<p class="block-title"><a href="http://astone.tv/events/week1804z/" target="_blank">和洋あれこれスイーツな<br>全国各教会の一週間</a></p>
-</div>
-
-<div class="column-inner">
 <span class="label-2">動画</span>
 <span>NEW</span>
 <a href="https://www.instagram.com/antiochmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのと東京アンテオケ教会'); "><img src="/2016/images/top/1803antiochhonobono.jpg"></a>
 <p class="block-date">動画ブログ：2018年4月23日～29日</p>
 <p class="block-title"><a href="https://www.instagram.com/antiochmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのと東京アンテオケ教会');">短い動画で見る<br>初夏の東京アンテオケ教会</p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/week1804z/" target="_blank"><img src="/2016/images/top/1804sweetszenkoku.jpg"></a>
+<p class="block-date">日程：2018年4月23日～29日</p>
+<p class="block-title"><a href="http://astone.tv/events/week1804z/" target="_blank">和洋あれこれスイーツな<br>全国各教会の一週間</a></p>
 </div>
 
 <div class="column-inner">
