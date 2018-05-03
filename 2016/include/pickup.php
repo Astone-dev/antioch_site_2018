3372dbfb112de6067aa9540a497ceea505f32c38
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-05-03 19:01:35 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -46,13 +46,6 @@
 <p class="block-title"><a href="http://astone.tv/events/1804meetings/" target="_blank">5月の爽やかな風の中関東を行く<br>川崎・町田宣教の旅<br>(日程：2018年5月4日～5日)</a></p>
 </div>-->
 
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/kyushu1805/" target="_blank"><img src="/2016/images/top/1708kyusyuseika.jpg"></a>
-<p class="block-date">日程：2018年4月29日～5月3日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/kyushu1805/" target="_blank">5月の爽やかな風の中九州を行く<br>九州宣教の旅<br>(日程：2018年4月30日～5月2日)</a></p>
-</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -195,6 +188,13 @@
 <a href="http://tokyo.antioch.jp/yorimichi/result-standup.html" target="_blank"><img src="/2016/images/top/1709yorimichi28kekka.jpg"></a>
 <p class="block-title"><a href="http://tokyo.antioch.jp/yorimichi/result-standup.html" target="_blank">ついに決定！<br>クリスチャンの寄り道 Vol.28<br>「倒れて立ち上がった経験」のお話し人気ベスト10<br>ぜひお聴きください。</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/kyushu1805/" target="_blank"><img src="/2016/images/top/1708kyusyuseika.jpg"></a>
+<p class="block-date">日程：2018年4月29日～5月3日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/kyushu1805/" target="_blank">5月の爽やかな風の中九州を行く<br>九州宣教の旅<br>(日程：2018年4月30日～5月2日)</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
