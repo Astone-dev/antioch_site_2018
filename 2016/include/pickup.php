c0c4315a1c8012a7db78643692bd5e00cca09389
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-05-04 00:22:14 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -26,10 +26,10 @@
 <div class="column-inner">
 <span class="label-2">映像　</span>
 <span>NEW</span>
-<a href="https://www.youtube.com/channel/UCEtTs2-eyqTCF5izdCMWvKA" target="_blank" onclick="javascript:pageTracker._trackPageview('吉祥寺J.GOSPEL FES聖会生中継');"><img src="/2016/images/top/1805kichijyojifeslive.png"></a>
+<a href="https://www.youtube.com/channel/UCEtTs2-eyqTCF5izdCMWvKA" target="_blank" onclick="javascript:pageTracker._trackPageview('吉祥寺J.GOSPEL FES生中継');"><img src="/2016/images/top/1805kichijyojifeslive.png"></a>
 <p class="block-date">2018年5月4日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCEtTs2-eyqTCF5izdCMWvKA" target="_blank" onclick="javascript:pageTracker._trackPageview('吉祥寺J.GOSPEL FES聖会生中継');">吉祥寺J.GOSPEL FES聖会生中継<br>時間 11:00〜16:00<br></a>
-<i style="color:#ff0000;font-size: 11px;">※ページ内のライブ配信中をタップしてごらんください</i></p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCEtTs2-eyqTCF5izdCMWvKA" target="_blank" onclick="javascript:pageTracker._trackPageview('吉祥寺J.GOSPEL FES生中継');">吉祥寺J.GOSPEL FES生中継<br>時間 11:00〜16:00<br></a>
+<i style="color:#ff0000;font-size: 11px;">※ページ内ライブ配信中をタップしてご覧んください</i></p>
 </div>
 
 <div class="column-inner">
