fe0dc3c658d3b600c2492d18a640f3d02f0e71b1
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-05-21 00:50:28 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -48,14 +48,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://astone.tv/events/2017byhisspirit/180520/" target="_blank"><img src="/2016/images/top/1709byhisnerima.jpg"></a>
-<p class="block-date">日程：2018年5月19日</p>
-<p class="block-title"><a href="http://astone.tv/events/2017byhisspirit/180520/" target="_blank">By His Spirit<br>世田谷の高齢者施設と<br>高円寺のレストランでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
 <span>NEW</span>
 <a href="/hakuba/" target="_blank"><img src="/2016/images/top/1705hakubacampinfo.jpg"></a>
 <p class="block-date">日程：2018年7月24日～7月27日</p>
@@ -72,7 +64,7 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/thanks1805/" target="_blank"><img src="/2016/images/top/1805sakushiyorokobi.jpg"></a>
+<a href="http://antiochblog.jp/praise/thanks1805/" target="_blank"><img src="/2016/images/top/1805shokazenkoku.jpg"></a>
 <p class="block-date">日程：2018年5月21日～27日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/thanks1805/" target="_blank">初夏の風が清々しい<br>全国各教会の一週間</a></p>
 </div>
@@ -163,6 +155,13 @@
 <a href="http://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1805chari_d.jpg"></a>
 <p class="block-date">日程：2018年5月6日</p>
 <p class="block-title"><a href="http://tokyo.antioch.jp/tcm/" target="_blank">東京カリスマ聖会<br>ダイジェスト写真集</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://astone.tv/events/2017byhisspirit/180520/" target="_blank"><img src="/2016/images/top/1709byhisnerima.jpg"></a>
+<p class="block-date">日程：2018年5月19日</p>
+<p class="block-title"><a href="http://astone.tv/events/2017byhisspirit/180520/" target="_blank">By His Spirit<br>世田谷の高齢者施設と<br>高円寺のレストランでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
