868567348a48f3476ddbbd9c37dca498b443f7e7
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-05-28 13:51:24 +0900

    PICKUP

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -38,7 +38,15 @@
 <span>NEW</span>
 <a href="http://antiochblog.jp/info/joy1805/" target="_blank"><img src="/2016/images/top/1805peppertyokuzen.jpg"></a>
 <p class="block-date">ブログ：2018年5月28日～6月3日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/joy1805/" target="_blank">いよいよ間近！<br>Pepper導入に向かって<br>アンテオケ教会の一週間</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/info/joy1805/" target="_blank">いよいよ間近！<br>Pepper導入に向かって<br>アンテオケ教会のわくわく</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://youtu.be/_ZDT8PSec8s" target="_blank"><img src="/2016/images/top/1805seishonohanashi_m.jpg"></a>
+<p class="block-date">ブログ：2018年5月28日～6月3日</p>
+<p class="block-title"><a href="https://youtu.be/_ZDT8PSec8s" target="_blank">世界で一番短い聖書の話し<br>紹介動画</a></p>
 </div>
 
 <div class="column-inner">
