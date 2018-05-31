a5d5449e961de3867e8b1e30afccbef68823830f
a-hisa0830 <johnfuji4845@gmail.com>
2018-05-31 16:40:10 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -82,9 +82,9 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<a href="http://antiochblog.jp/info/jerusalem2017/180531/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
 <p class="block-date"> 日程：2018年5月31日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/" target="_blank">エルサレム<br>田無のデイサービスでゴスペルコンサート</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180531/" target="_blank">エルサレム<br>田無のデイサービスでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
