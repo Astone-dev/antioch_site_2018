6050f3bc912e6ddb5c451c08e5a4eeec1ad85081
a-hisa0830 <johnfuji4845@gmail.com>
2018-06-03 23:26:48 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -24,6 +24,30 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/chugoku1806/" target="_blank">青葉薫る中国地方を行く<br>中国宣教の旅<br>(日程：2018年6月4日〜6日)</a></p>
 </div>
 
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/june2018/" target="_blank"><img src="/2016/images/top/1805tsuyutyokuantioch.jpg"></a>
+<p class="block-date">ブログ：2018年6月4日～10日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/june2018/" target="_blank">白馬キャンプに向かって<br>東京アンテオケ教会メンバー<br>ワクワクな一週間（思い出を振り返りつつ）</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/hibiki1806/" target="_blank"><img src="/2016/images/top/1805tsuyutyokuantioch.jpg"></a>
+<p class="block-date">ブログ：2018年6月4日～10日</p>
+<p class="block-title"><a href="http://astone.tv/events/hibiki1806/" target="_blank">風のひびき第6週募集開始<br>作詞作曲者たちの驚くべき野心</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/pepper1806/" target="_blank"><img src="/2016/images/top/1805tsuyutyokuantioch.jpg"></a>
+<p class="block-date">ブログ：2018年6月4日～10日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/pepper1806/" target="_blank">いよいよPepper来る！</a></p>
+</div>-->
+
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
@@ -78,6 +102,14 @@
 <p class="block-date">動画ブログ：2018年5月28日～6月3日</p>
 <p class="block-title"><a href="https://www.instagram.com/antiochmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのと東京アンテオケ教会');">短い動画で見る<br>初夏の東京アンテオケ教会</a></p>
 </div>
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/june2018z/" target="_blank"><img src="/2016/images/top/1805tsuyutyokuantioch.jpg"></a>
+<p class="block-date">ブログ：2018年6月4日～10日</p>
+<p class="block-title"><a href="http://astone.tv/events/june2018z/" target="_blank">白馬キャンプに向かって<br>全国各教会<br>ワクワクな一週間（思い出を振り返りつつ）</a></p>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
