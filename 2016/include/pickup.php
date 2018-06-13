70ed0aa175477380bda07df70c88dce0a7b0ecca
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-06-13 17:57:05 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -72,14 +72,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank"><img src="/2016/images/top/1701jerukawaguchi.jpg"></a>
-<p class="block-date"> ブログ：2018年6月12日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank">エルサレム<br>三郷市アカシアの家グループホームでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
 <span class="label-2">動画</span>
 <span>NEW</span>
 <a href="https://www.instagram.com/tlcccmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのとTLCCC全国各教会'); "><img src="/2016/images/top/1803zenkokuhonobono.jpg"></a>
@@ -149,6 +141,13 @@
 <a href="/yorimichi/result-standup.html" target="_blank"><img src="/2016/images/top/1709yorimichi28kekka.jpg"></a>
 <p class="block-title"><a href="/yorimichi/result-standup.html" target="_blank">ついに決定！<br>クリスチャンの寄り道 Vol.28<br>「倒れて立ち上がった経験」のお話し人気ベスト10<br>ぜひお聴きください。</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank"><img src="/2016/images/top/1701jerukawaguchi.jpg"></a>
+<p class="block-date"> ブログ：2018年6月12日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank">エルサレム<br>三郷市アカシアの家グループホームでゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
