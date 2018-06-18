ec9c3ce7706a7dc312028408732ab6bb1ea7e2cf
a-hisa0830 <johnfuji4845@gmail.com>
2018-06-18 12:30:05 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -80,6 +80,14 @@
 </div>
 
 <div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180618/" target="_blank"><img src="/2016/images/top/1701jerukawaguchi.jpg"></a>
+<p class="block-date"> ブログ：2018年6月18日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180618/" target="_blank">エルサレム<br>三郷のグループホームでゴスペル教室</a></p>
+</div>
+
+<div class="column-inner">
 <span class="label-2">動画</span>
 <span>NEW</span>
 <a href="https://www.instagram.com/tlcccmovielog/" target="_blank" onclick="javascript:pageTracker._trackPageview('短い動画で見る春を感じてほのぼのとTLCCC全国各教会'); "><img src="/2016/images/top/1803zenkokuhonobono.jpg"></a>
@@ -227,12 +235,12 @@
 <p class="block-title"><a href="http://antiochblog.jp/praise/2017labeille/180616/" target="_blank">ラベイユ<br>荒川区の福祉施設でゴスペルコンサート</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/180616/" target="_blank"><img src="/2016/images/top/1701jerukawaguchi.jpg"></a>
 <p class="block-date"> ブログ：2018年6月16日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180616/" target="_blank">エルサレム<br>さいたま市岩槻デイケアでゴスペルコンサート</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -241,12 +249,12 @@
 <p class="block-title"><a href="http://antiochblog.jp/info/gracious/180616/" target="_blank">GRACIOUS DOORS<br>都内デイサービスでゴスペルコンサート</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank"><img src="/2016/images/top/1701jerukawaguchi.jpg"></a>
 <p class="block-date"> ブログ：2018年6月12日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180612/" target="_blank">エルサレム<br>三郷市アカシアの家グループホームでゴスペルコンサート</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
