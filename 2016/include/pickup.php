b4719b108422b2b057ad19184357b6c5caa3dbed
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-07-02 13:43:10 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -20,7 +20,7 @@
 <span class="label-2">動画</span>
 <span>NEW</span>
 <a href="http://tv.antioch.jp/testimony/category/miracle/" target="_blank"><img src="/2016/images/top/1806kyounokiseki.jpg"></a>
-<p class="block-title"><a href="http://tv.antioch.jp/testimony/category/miracle/" target="_blank">今日のキセキ</a></p>
+<p class="block-title"><a href="http://tv.antioch.jp/testimony/category/miracle/" target="_blank">今日のキセキ<br>日常におこった小さなキセキ</a></p>
 </div>
 
 <div class="column-inner">
