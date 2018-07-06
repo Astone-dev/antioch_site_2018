372c09abd6f5cefbe7006965cdd08e4f5c366a30
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-07-06 19:17:45 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -58,14 +58,6 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/1807true/" target="_blank"><img src="/2016/images/top/1803truevinehikoshi.jpg"></a>
-<p class="block-date">ブログ：2018年7月5日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1807true/" target="_blank">TrueVine<br>東久留米の施設でゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="https://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1806tocharidigest_p.jpg"></a>
 <p class="block-date">ブログ：2018年7月4日～</p>
 <p class="block-title"><a href="https://tokyo.antioch.jp/tcm/" target="_blank">6月の東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)</a></p>
@@ -151,6 +143,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/1807true/" target="_blank"><img src="/2016/images/top/1803truevinehikoshi.jpg"></a>
+<p class="block-date">ブログ：2018年7月5日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1807true/" target="_blank">TrueVine<br>東久留米の施設でゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
