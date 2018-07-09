8ef2136745a129ec7e4b438863851e2acb60c503
a-hisa0830 <johnfuji4845@gmail.com>
2018-07-09 23:05:07 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -46,14 +46,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/1807kawaguchi/" target="_blank"><img src="/2016/images/top/1704patolisshisetsu.jpg"></a>
-<p class="block-date">ブログ：2018年7月9日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/1807kawaguchi/" target="_blank">PATOLIS <br>川口の病院デイケアでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
 <span>NEW</span>
 <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
 <p class="block-title"><a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki');">miki<br>(Instagram)</a></p>
@@ -97,10 +89,10 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/180709/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
-<p class="block-date">ブログ：2018年7月9日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180709/" target="_blank">エルサレム<br>三郷市のグループホームでゴスペル教室</a></p>
-</div>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180710/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年7月10日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180710/" target="_blank">エルサレム<br>三郷市グループホームでゴスペルコンサート</a></p></div>
 
 <div class="column-inner">
 <span class="label-2">動画</span>
@@ -154,6 +146,23 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/1807kawaguchi/" target="_blank"><img src="/2016/images/top/1704patolisshisetsu.jpg"></a>
+<p class="block-date">ブログ：2018年7月9日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/1807kawaguchi/" target="_blank">PATOLIS <br>川口の病院デイケアでゴスペルコンサート</a></p>
+</div>
+
+
+<!--<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180709/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年7月9日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180709/" target="_blank">エルサレム<br>三郷市のグループホームでゴスペル教室</a></p>
+</div>-->
+
+
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
@@ -211,12 +220,12 @@
 <p class="block-title"><a href="http://astone.tv/events/1807meetings/" target="_blank">雨模様の週末<br>川崎、町田宣教の旅<br>(日程 2018年7月6日～7日)</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
 <span class="label-1">ブログ</span>
 <a href="http://antiochblog.jp/info/jerusalem2017/180707/" target="_blank"><img src="/2016/images/top/1705jerumisato.jpg"></a>
 <p class="block-date">ブログ：2018年7月7日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180707/" target="_blank">エルサレム<br>さいたま市岩槻の特別養護老人ホームでゴスペルコンサート</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
