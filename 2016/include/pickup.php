b447b20e439a64442d7d913dc5b34904ac623f64
a-hisa0830 <johnfuji4845@gmail.com>
2018-07-15 00:10:59 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -52,6 +52,14 @@
 <p class="block-date"> ブログ：2018年7月15日～16日</p>
 <p class="block-title"><a href="http://antiochblog.jp/praise/gospel1807/" target="_blank">J-GOSPEL Fes.に向かって <br>出演者の２日間</a></p></div>
 
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/praise/2018women/201807/" target="_blank"><img src="/2016/images/top/1704fujinkai.jpg"></a>
+<p class="block-date"> ブログ：2018年7月15日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/2018women/201807/" target="_blank">婦人会</a></p></div>
+
 <div class="column-inner">
 <span class="label-2">動画</span>
 <span>NEW</span>
