55b9aa748e0ba40e0b14f618b41f21d24c113dc2
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-08-04 10:59:30 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -58,7 +58,7 @@
 <span>NEW</span>
 <a href="http://astone.tv/events/1808live/" target="_blank"><img src="/2016/images/top/1706ikebukuro.jpg"></a>
 <p class="block-date">ブログ：2018年8月3日～8月4日</p>
-<p class="block-title"><a href="http://astone.tv/events/1808live/" target="_blank">池袋J.GOSPELフェスに向かって<br>参加グループとThe Visionスタッフの2日間</a></p>
+<p class="block-title"><a href="http://astone.tv/events/1808live/" target="_blank">池袋J.GOSPEL LIVEに向かって<br>参加グループとThe Visionスタッフの2日間</a></p>
 </div>
 
 <div class="column-inner">
