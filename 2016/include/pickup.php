85da99a8f7107e0679181eddad5612953bd37c56
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-08-04 09:10:23 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -135,6 +135,14 @@
 <p class="block-title"><a href="http://astone.tv/events/summer2018z/" target="_blank">夏真っ盛り！<br>全国各教会の一週間</a></p>
 </div>
 
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180804/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年8月4日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180804/" target="_blank">エルサレム<br>さいたま市のグループホームでゴスペルコンサート</a></p>
+</div>
+
 <!--<div class="column-inner">
 <span>NEW</span>
 <a href="/hakuba/" target="_blank"><img src="/2016/images/top/1705hakubacampinfo.jpg"></a>
