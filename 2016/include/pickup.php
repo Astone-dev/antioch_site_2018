d53b68442b2e2ea2aad6439a2a0e3e99a4ca0141
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-08-13 15:33:06 +0900

    pick up修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -83,9 +83,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/180813/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<a href="http://antiochblog.jp/info/jerusalem2017/20180813/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
 <p class="block-date">ブログ：2018年8月13日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180813/" target="_blank">エルサレム<br>三郷の施設でゴスペルコンサート</a></p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/20180813/" target="_blank">エルサレム<br>三郷の施設でゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
