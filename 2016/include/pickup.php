b64db428202d7323f288cbee5fd1ea505bf9dcc9
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-08-22 01:00:13 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -48,9 +48,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/praise/song6th201808/" target="_blank"><img src="/2016/images/top/1708familyday.jpg"></a>
+<a href="http://astone.tv/events/familycamp201808/" target="_blank"><img src="/2016/images/top/1708familyday.jpg"></a>
 <p class="block-date">ブログ：2018年8月22日～23日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/song6th201808/" target="_blank">いよいよ明日！<br>秋川渓谷で大暴れ！<br>ファミリーデイキャンプ<br>(日程：2018年8月23日)</a></p>
+<p class="block-title"><a href="http://astone.tv/events/familycamp201808/" target="_blank">いよいよ明日！<br>秋川渓谷で大暴れ！<br>ファミリーデイキャンプ<br>(日程：2018年8月23日)</a></p>
 </div>
 
 <div class="column-inner">
