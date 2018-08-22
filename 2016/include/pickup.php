da44a2239791dc5bab4c6b3ae5f10f67dc3e1902
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-08-22 12:11:09 +0900

    pick up修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -25,8 +25,8 @@
 <div class="column-inner">
 <span class="label-2">動画</span>
 <span>NEW</span>
-<a href="http://tv.antioch.jp/testimony/" target="_blank"><img src="/2016/images/top/1808kiseki_yui.jpg"></a>
-<p class="block-title"><a href="http://tv.antioch.jp/testimony/" target="_blank">今日のキセキ<br>日常におこった小さなキセキ<br>(2018年8月20日UP!)</a></p>
+<a href="http://tv.antioch.jp/testimony/category/miracle/" target="_blank"><img src="/2016/images/top/1808kiseki_yui.jpg"></a>
+<p class="block-title"><a href="http://tv.antioch.jp/testimony/category/miracle/" target="_blank">今日のキセキ<br>日常におこった小さなキセキ<br>(2018年8月20日UP!)</a></p>
 </div>
 
 <div class="column-inner">
