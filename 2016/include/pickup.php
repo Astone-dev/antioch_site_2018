8f422c6f2a31008375eca9fcd6e82117499a0900
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-08-23 01:13:23 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -40,17 +40,17 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://antiochblog.jp/info/thanks1808/" target="_blank"><img src="/2016/images/top/1709akimikakuantioch.jpg"></a>
-<p class="block-date">ブログ：2018年8月20日～26日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/thanks1808/" target="_blank">秋の訪れを感じつつ<br>東京アンテオケの一週間</a></p>
+<a href="http://astone.tv/events/familycamp201808/" target="_blank"><img src="/2016/images/top/1708familyday.jpg"></a>
+<p class="block-date">ブログ：2018年8月22日～23日</p>
+<p class="block-title"><a href="http://astone.tv/events/familycamp201808/" target="_blank">秋川渓谷で大暴れ！<br>ファミリーデイキャンプ<br>(日程：2018年8月23日)</a></p>
 </div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://astone.tv/events/familycamp201808/" target="_blank"><img src="/2016/images/top/1708familyday.jpg"></a>
-<p class="block-date">ブログ：2018年8月22日～23日</p>
-<p class="block-title"><a href="http://astone.tv/events/familycamp201808/" target="_blank">いよいよ明日！<br>秋川渓谷で大暴れ！<br>ファミリーデイキャンプ<br>(日程：2018年8月23日)</a></p>
+<a href="http://antiochblog.jp/info/thanks1808/" target="_blank"><img src="/2016/images/top/1709akimikakuantioch.jpg"></a>
+<p class="block-date">ブログ：2018年8月20日～26日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/thanks1808/" target="_blank">秋の訪れを感じつつ<br>東京アンテオケの一週間</a></p>
 </div>
 
 <div class="column-inner">
