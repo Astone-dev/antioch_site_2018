04e6c8e5dd367c87bc863f588b712dfa80144133
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-08-26 18:15:28 +0900

    pick up修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -205,7 +205,6 @@
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
-<span>NEW</span>
 <a href="http://astone.tv/events/familycamp201808/" target="_blank"><img src="/2016/images/top/1708familyday.jpg"></a>
 <p class="block-date">ブログ：2018年8月22日～23日</p>
 <p class="block-title"><a href="http://astone.tv/events/familycamp201808/" target="_blank">秋川渓谷で大暴れ！<br>ファミリーデイキャンプ<br>(日程：2018年8月23日)</a></p>
