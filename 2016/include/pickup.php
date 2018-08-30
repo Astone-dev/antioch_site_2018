b9553e121d25ff4e2904ad0fe29595b87ef4e305
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-08-31 00:45:24 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -54,14 +54,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/praise/patolis/180829/" target="_blank"><img src="/2016/images/top/1606patolisnerima.jpg"></a>
-<p class="block-date">ブログ：2018年8月29日</p>
-<p class="block-title"><a href="http://antiochblog.jp/praise/patolis/180829/" target="_blank">PATOLIS<br>川口の病院でゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
 <span>NEW</span>
 <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
 <p class="block-title"><a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki');">miki<br>(Instagram)</a></p>
@@ -141,6 +133,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/praise/patolis/180829/" target="_blank"><img src="/2016/images/top/1606patolisnerima.jpg"></a>
+<p class="block-date">ブログ：2018年8月29日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/patolis/180829/" target="_blank">PATOLIS<br>川口の病院でゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-2">動画</span>
