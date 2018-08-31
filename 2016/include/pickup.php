1ab201ad0b4a5a6e0980d4f60238758a2241e982
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-09-01 02:46:56 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -32,9 +32,9 @@
 <div class="column-inner">
 <span class="label-1">ブログ</span>
 <span>NEW</span>
-<a href="http://blog.antioch.jp/hokkaido1808/" target="_blank"><img src="/2016/images/top/1708hokkaidou.jpg"></a>
-<p class="block-date">ブログ：2018年8月26日～31日</p>
-<p class="block-title"><a href="http://blog.antioch.jp/hokkaido1808/" target="_blank">秋の気配<br>北海道宣教の旅<br>(日程：2018年8月27日～31日)</a></p>
+<a href="http://antiochblog.jp/praise/charisma1809/" target="_blank"><img src="/2016/images/top/1809tokyocharisma.jpg"></a>
+<p class="block-date">ブログ：2018年9月1日〜2日</p>
+<p class="block-title"><a href="http://antiochblog.jp/praise/charisma1809/" target="_blank">大忙し<br>東京アンテオケの裏と表舞台<br>(日程：2018年9月2日)</a></p>
 </div>
 
 <div class="column-inner">
@@ -133,6 +133,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://blog.antioch.jp/hokkaido1808/" target="_blank"><img src="/2016/images/top/1708hokkaidou.jpg"></a>
+<p class="block-date">ブログ：2018年8月26日～31日</p>
+<p class="block-title"><a href="http://blog.antioch.jp/hokkaido1808/" target="_blank">秋の気配<br>北海道宣教の旅<br>(日程：2018年8月27日～31日)</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
