99a975eb2ea3c179865ff7dc973fc7cf56f93f1c
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-09-02 00:33:23 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -54,6 +54,14 @@
 </div>
 
 <div class="column-inner">
+<span class="label-1">ブログ</span>
+<span>NEW</span>
+<a href="http://astone.tv/events/euqaristias/20180902shinjuku/" target="_blank"><img src="/2016/images/top/1612eukarishinjyuku.jpg"></a>
+<p class="block-date">ブログ：2018年9月2日</p>
+<p class="block-title"><a href="http://astone.tv/events/euqaristias/20180902shinjuku/" target="_blank">エウカリスティアス<br>新宿のデイサービスでゴスペル</a></p>
+</div>
+
+<div class="column-inner">
 <span>NEW</span>
 <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
 <p class="block-title"><a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki');">miki<br>(Instagram)</a></p>
@@ -80,14 +88,6 @@
 <a href="http://antiochblog.jp/info/gogo1808z/" target="_blank"><img src="/2016/images/top/1708mousugushingakizenkoku.jpg"></a>
 <p class="block-date">ブログ：2018年8月27日～9月2日</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/gogo1808z/" target="_blank">夏休みもこれで終わり<br>全国各教会</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-1">ブログ</span>
-<span>NEW</span>
-<a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
-<p class="block-date">ブログ：2018年9月1日</p>
-<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank">エルサレム<br>岩槻西のグループホームでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
@@ -141,6 +141,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+<span class="label-1">ブログ</span>
+<a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+<p class="block-date">ブログ：2018年9月1日</p>
+<p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank">エルサレム<br>岩槻西のグループホームでゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
 <span class="label-1">ブログ</span>
