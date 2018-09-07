ea8888f51d79f51fc83b4cb60d382cca4472cef2
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-09-07 23:05:34 +0900

    聖会案内更新

--- 2016/include/akimoto_seikai.php
+++ 2016/include/akimoto_seikai.php
@@ -1,6 +1,6 @@
                 <h3>パウロ秋元牧師 次回の聖会</h3>
                 <ul>   
-                  <li>決まり次第掲載致します。</li>                  
+                  <li>9/8 横浜　18:30</li>                  
 
-                  <!--<p><a href="/meetings/akimoto.html">→詳しくはこちらをクリック</a></p>-->
+                  <p><a href="/meetings/akimoto.html">→詳しくはこちらをクリック</a></p>
                 </ul>
