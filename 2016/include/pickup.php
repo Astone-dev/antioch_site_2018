25b856d2b9dbe2d49feda1d63cd0c58af18182c8
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-09-10 23:04:24 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -61,14 +61,6 @@
 </div>
 
 <div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://astone.tv/events/euqaristias/20180910septemberconcert/" target="_blank"><img src="/2016/images/top/1803euqarinerima.jpg"></a>
-    <p class="block-date">ブログ：2018年9月10日</p>
-    <p class="block-title"><a href="http://astone.tv/events/euqaristias/20180910septemberconcert/" target="_blank">エウカリスティアス<br>セプテンバーコンサートに出演</a></p>
-</div>
-
-<div class="column-inner">
     <span>NEW</span>
     <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
     <p class="block-title"><a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki');">miki<br>(Instagram)</a></p>
@@ -89,13 +81,13 @@
     <p class="block-title"><a href="http://astone.tv/events/yobi1809z/" target="_blank">秋近づく<br>全国各教会</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/jerusalem2017/180911/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
     <p class="block-date">ブログ：2018年9月11日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180911/" target="_blank">エルサレム <br>埼玉県三郷市アカシアの家オレンジカフェでコンサート</a></p>
-</div>-->
+</div>
 
 <div class="column-inner">
     <span>NEW</span>
@@ -148,6 +140,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://astone.tv/events/euqaristias/20180910septemberconcert/" target="_blank"><img src="/2016/images/top/1803euqarinerima.jpg"></a>
+    <p class="block-date">ブログ：2018年9月10日</p>
+    <p class="block-title"><a href="http://astone.tv/events/euqaristias/20180910septemberconcert/" target="_blank">エウカリスティアス<br>セプテンバーコンサートに出演</a></p>
+</div>
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
@@ -219,12 +218,12 @@
     <p class="block-title"><a href="http://astone.tv/events/1808song/" target="_blank">風のひびき6集 応募曲378曲 <br>審査員激闘の一週間</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <a href="http://astone.tv/events/euqaristias/20180902shinjuku/" target="_blank"><img src="/2016/images/top/1612eukarishinjyuku.jpg"></a>
     <p class="block-date">ブログ：2018年9月2日</p>
     <p class="block-title"><a href="http://astone.tv/events/euqaristias/20180902shinjuku/" target="_blank">エウカリスティアス<br>新宿のデイサービスでゴスペル</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
@@ -240,12 +239,12 @@
     <p class="block-title"><a href="https://tokyo.antioch.jp/tcm/" target="_blank">8月東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
     <p class="block-date">ブログ：2018年9月1日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180901/" target="_blank">エルサレム<br>岩槻西のグループホームでゴスペルコンサート</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
