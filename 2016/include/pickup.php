6a2f7d3e368b657f0b682eca5db6a8b89bc87e66
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-09-18 08:17:47 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -91,7 +91,7 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/info/jerusalem2017/180918/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+    <a href="http://antiochblog.jp/info/jerusalem2017/180918/" target="_blank"><img src="/2016/images/top/1605jeruhakone.jpg"></a>
     <p class="block-date">ブログ：2018年9月18日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/180918/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
 </div>
