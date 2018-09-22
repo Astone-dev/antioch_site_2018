42bc039ba54306efa1436ec7d879d6e0e5a31473
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-09-22 23:07:04 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -46,22 +46,6 @@
 </div>
 
 <div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/gospel2017/201809/" target="_blank"><img src="/2016/images/top/1710gospeltudoi.jpg"></a>
-    <p class="block-date">ブログ：2018年9月22日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/gospel2017/201809/" target="_blank">9月ゴスペルの集いの一日</a></p>
-</div>
-
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('ゴスペルの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2018年9月22日 18:30～</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('ゴスペルの集い-生中継');">ゴスペルの集い 生中継<br>(日程：2018年9月22日 18:30スタート)</a></p>
-</div>
-
-<div class="column-inner">
     <span>NEW</span>
     <a href="/pray4jerusalem/" target="_blank"><img src="/2016/images/top/1709peaceforJerusalem.jpg"></a>
     <p class="block-title"><a href="/pray4jerusalem/" target="_blank">エルサレムの平和を祈る集い<br>ご案内<br>(日程：2018年10月7日)</a></p>
@@ -162,6 +146,13 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/gospel2017/201809/" target="_blank"><img src="/2016/images/top/1710gospeltudoi.jpg"></a>
+    <p class="block-date">ブログ：2018年9月22日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/gospel2017/201809/" target="_blank">9月ゴスペルの集いの一日</a></p>
+</div>
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
