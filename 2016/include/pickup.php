f8254e71d41a2dfa9d585ee278ad5cf1dfc6ef6e
takuya yoshida <sameltakuya@gmail.com>
2018-09-24 22:58:55 +0900

    ピックアップ 現地時間追加

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -42,7 +42,7 @@
 <span>NEW</span>
 <a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');"><img src="/2016/images/top/1610hanbokushilive.png"></a>
 <p class="block-date">2018年9月24、25日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');">カンボジアチーム 聖会生中継<br>9月24日 日本時間21:00頃／19:00頃<br>9月25日 日本時間21:00頃／19:00頃<br></a>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew" target="_blank" onclick="javascript:pageTracker._trackPageview('カンボジアチーム聖会生中継');">カンボジアチーム 聖会生中継<br>9月24日 日本時間21:00頃 ／ 現地時間19:00頃<br>9月25日 日本時間21:00頃 ／ 現地時間19:00頃<br></a>
 <i style="color:#ff0000;font-size: 11px;">※ページ内のライブ配信中をタップしてごらんください</i></p>
 </div>
 
