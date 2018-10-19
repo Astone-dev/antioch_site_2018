786c0e92d433e25a497a5d18166e7e5a9415b16d
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-10-19 23:45:40 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -62,6 +62,22 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/info/revival2017/181020/" target="_blank"><img src="/2016/images/top/1704revivalnight.jpg"></a>
+    <p class="block-date">ブログ：2018年10月20日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/revival2017/181020/" target="_blank">Revival Nightの一日</a></p>
+</div>
+
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('Revival Night生中継'); "><img src="/2016/images/top/1610revivalnightlive.png"></a>
+<p class="block-date">2018年10月20日</p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('Revival Night生中継');">Revival Night生中継<br>(日程：2018年10月20日 18:30スタート)</a></p>
+</div>
+
+<div class="column-inner">
     <span class="label-2">動画</span>
     <span>NEW</span>
     <a href="https://tokyo.antioch.jp/internet-tv/181014-yosen.html" target="_blank"><img src="/2016/images/top/1810nenmatsucon_m.jpg"></a>
@@ -95,6 +111,14 @@
     <a href="http://astone.tv/events/faith1810z/" target="_blank"><img src="/2016/images/top/1709akimikakuzenkoku.jpg"></a>
     <p class="block-date">ブログ：2018年10月15日～21日</p>
     <p class="block-title"><a href="http://astone.tv/events/faith1810z/" target="_blank">食欲の秋<br>ウキウキ全国各教会の一週間</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/praise/gracejewel2017/181020/" target="_blank"><img src="/2016/images/top/1704gracegospel.jpg"></a>
+    <p class="block-date">ブログ：2018年10月20日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/181020/" target="_blank">GRACEと宝石 <br>東中野のデイサービスでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
