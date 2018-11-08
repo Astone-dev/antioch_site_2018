73b0ad70a109cc7c78b7dc9169874e426559f1f5
takuya yoshida <sameltakuya@gmail.com>
2018-11-08 09:49:24 +0900

    GRACEと宝石 高円寺のデイサービスで奉仕

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -68,6 +68,14 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank"><img src="/2016/images/top/1708gracehosekifutyu.jpg"></a>
+    <p class="block-date">日程：2018年11月8日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank">GRACEと宝石 高円寺のデイサービスで奉仕</a></p>
+</div>
+
+<div class="column-inner">
     <span>NEW</span>
     <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
     <p class="block-title"><a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki');">miki<br>(Instagram)</a></p>
