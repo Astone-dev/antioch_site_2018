e16180d4c8d319e7a2b82170737598348637ca27
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-11-12 03:15:17 +0900

    pickup 更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -39,34 +39,10 @@
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://blog.antioch.jp/1811israel/" target="_blank"><img src="/2016/images/top/1610israelchokuzen.jpg"></a>
-    <p class="block-date">日程：2018年10月29日〜11月10日</p>
-    <p class="block-title"><a href="http://blog.antioch.jp/1811israel/" target="_blank">いよいよ出発<br>イスラエルチーム<br>最新情報写真集<br>(チーム日程：2018年10月29日〜11月10日)</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/charisma1811/" target="_blank"><img src="/2016/images/top/1809tokyocharisma.jpg"></a>
-    <p class="block-date">日程：2018年11月10日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/charisma1811/" target="_blank">11月 カリスマ聖会</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://astone.tv/events/2017healing/" target="_blank"><img src="/2016/images/top/1609iyashinotsudoi.jpg"></a>
-    <p class="block-date">日程：2018年11月10日</p>
-    <p class="block-title"><a href="http://astone.tv/events/2017healing/" target="_blank">いやしの集いの一日</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/start201811/" target="_blank"><img src="/2016/images/top/1711shinnendoantioch.jpg"></a>
-    <p class="block-date">日程：2018年11月5日～11月11日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/start201811/" target="_blank">東京アンテオケ教会<br>新年度スタートの一週間</a></p>
+    <span>NEW</span>    
+    <a href="http://antiochblog.jp/praise/nov2018/" target="_blank"><img src="/2016/images/top/1610gospelantioch.jpg"></a>
+    <p class="block-date">日程：2018年11月12日～11月18日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/nov2018/" target="_blank">東京アンテオケ教会</a></p>
 </div>
 
 <div class="column-inner">
@@ -81,33 +57,6 @@
     <span>NEW</span>
     <a href="/tcm/" target="_blank" onclick="javascript:pageTracker._trackPageview('tcm'); "><img src="/2016/images/top/1809tokyocharisma.jpg"></a>
     <p class="block-title"><a href="/tcm/" target="_blank" onclick="javascript:pageTracker._trackPageview('tcm');">10月の東京カリスマ聖会<br>ダイジェスト写真<br>きれいな写真版</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/info/jerusalem2017/181110/" target="_blank"><img src="/2016/images/top/1804jerhakone.jpg"></a>
-    <p class="block-date">日程：2018年11月10日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/181110/" target="_blank">エルサレム<br>福島県相馬市レストランにてコンサート</a></p>
-</div>
-
-
-<!--
-<div class="column-inner">
-<span class="label-2">映像</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2018年8月11日</p>
-<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継');">いやしの集い 生中継<br> (日程：2018年8月11日 18:30スタート)</a></p>
-</div>-->
-
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank"><img src="/2016/images/top/1708gracehosekifutyu.jpg"></a>
-    <p class="block-date">日程：2018年11月8日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank">GRACEと宝石 高円寺のデイサービスで奉仕</a></p>
 </div>
 
 <div class="column-inner">
@@ -126,9 +75,9 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://astone.tv/events/start201811z/" target="_blank"><img src="/2016/images/top/1711shinnendozenkoku.jpg"></a>
-    <p class="block-date">日程：11月5日～11月11日</p>
-    <p class="block-title"><a href="http://astone.tv/events/start201811z/" target="_blank">主の十字架の全国各教会<br>新年度スタート一週間</a></p>
+    <a href="http://astone.tv/events/nov2018z/" target="_blank"><img src="/2016/images/top/1610gospelzenkoku.jpg"></a>
+    <p class="block-date">日程：11月12日～11月18日</p>
+    <p class="block-title"><a href="http://astone.tv/events/nov2018z/" target="_blank">主の十字架の全国各教会</a></p>
 </div>
 
 <div class="column-inner">
@@ -196,6 +145,66 @@
 <p class="block-date">毎日7:00更新</p>
 <p class="block-title"><a href="http://antiochblog.jp/info/kangekishitakoto/" target="_blank">クリスチャンの寄り道 Vol.32<br>今回は「神さまのことで一番感激したこと」のお話です</a></p>
 </div>-->
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://blog.antioch.jp/1811israel/" target="_blank"><img src="/2016/images/top/1610israelchokuzen.jpg"></a>
+    <p class="block-date">日程：2018年10月29日〜11月10日</p>
+    <p class="block-title"><a href="http://blog.antioch.jp/1811israel/" target="_blank">いよいよ出発<br>イスラエルチーム<br>最新情報写真集<br>(チーム日程：2018年10月29日〜11月10日)</a></p>
+</div>
+
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/charisma1811/" target="_blank"><img src="/2016/images/top/1809tokyocharisma.jpg"></a>
+    <p class="block-date">日程：2018年11月10日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/charisma1811/" target="_blank">11月 カリスマ聖会</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/start201811/" target="_blank"><img src="/2016/images/top/1711shinnendoantioch.jpg"></a>
+    <p class="block-date">日程：2018年11月5日～11月11日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/start201811/" target="_blank">東京アンテオケ教会<br>新年度スタートの一週間</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://astone.tv/events/start201811z/" target="_blank"><img src="/2016/images/top/1711shinnendozenkoku.jpg"></a>
+    <p class="block-date">日程：11月5日～11月11日</p>
+    <p class="block-title"><a href="http://astone.tv/events/start201811z/" target="_blank">主の十字架の全国各教会<br>新年度スタート一週間</a></p>
+</div>
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://astone.tv/events/2017healing/" target="_blank"><img src="/2016/images/top/1609iyashinotsudoi.jpg"></a>
+    <p class="block-date">日程：2018年11月10日</p>
+    <p class="block-title"><a href="http://astone.tv/events/2017healing/" target="_blank">いやしの集いの一日</a></p>
+</div>
+
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/info/jerusalem2017/181110/" target="_blank"><img src="/2016/images/top/1804jerhakone.jpg"></a>
+    <p class="block-date">日程：2018年11月10日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/181110/" target="_blank">エルサレム<br>福島県相馬市レストランにてコンサート</a></p>
+</div>
+
+
+<!--
+<div class="column-inner">
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継'); "><img src="/2016/images/top/1610hanbokushilive.png"></a>
+<p class="block-date">2018年8月11日</p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('いやしの集い-生中継');">いやしの集い 生中継<br> (日程：2018年8月11日 18:30スタート)</a></p>
+</div>-->
+
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank"><img src="/2016/images/top/1708gracehosekifutyu.jpg"></a>
+    <p class="block-date">日程：2018年11月8日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/181108/" target="_blank">GRACEと宝石 高円寺のデイサービスで奉仕</a></p>
+</div>
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
