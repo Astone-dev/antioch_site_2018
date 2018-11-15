466da22a541ab3450033b40f9b1f7039071f6a22
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2018-11-16 00:36:06 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -60,6 +60,14 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://astone.tv/events/gate2018/181116/" target="_blank"><img src="/2016/images/top/1704gateofhigashikurume.jpg"></a>
+    <p class="block-date">日程：2018年11月16日</p>
+    <p class="block-title"><a href="http://astone.tv/events/gate2018/181116/" target="_blank">The Gate of Praise<br>練馬区の老健でゴスペルコンサート</a></p>
+</div>
+
+<div class="column-inner">
     <span class="label-2">動画</span>
     <span>NEW</span>
     <a href="https://www.youtube.com/watch?v=rHeGvBKd3aY&t=205s" target="_blank" onclick="javascript:pageTracker._trackPageview('GiftsTV'); "><img src="/2016/images/top/181030giftstv_.jpg"></a>
