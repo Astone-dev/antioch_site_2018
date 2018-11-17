5a6d3d273283ad5dbbe1b0d90d3622823f232137
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-11-17 14:52:01 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -69,6 +69,14 @@
     <p class="block-title"><a href="http://antiochblog.jp/praise/patolis/181117/" target="_blank">PATOLIS<br>川口の病院、病棟でのコンサート</a></p>
 </div>
 
+<!--<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>    
+    <a href="http://astone.tv/events/euqaristias/20181118gospelconcert/" target="_blank"><img src="/2016/images/top/1610eukarishinjyuku.jpg"></a>
+    <p class="block-date">日程：2018年11月18日</p>
+    <p class="block-title"><a href="http://astone.tv/events/euqaristias/20181118gospelconcert/" target="_blank">Euqaristias<br>新宿のデイサービスでゴスペルコンサート</a></p>
+</div>-->
+
 <div class="column-inner">
     <span>NEW</span>
     <a href="https://www.instagram.com/miki171717/" target="_blank" onclick="javascript:pageTracker._trackPageview('Instagram-miki'); "><img src="/2016/images/top/1806miki171717.jpg"></a>
