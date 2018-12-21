920b915b47c05eee2f294087af7d0d2254a22069
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2018-12-22 00:41:26 +0900

    pick up修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -259,7 +259,6 @@
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
-    <span>NEW</span>
     <a href="http://antiochblog.jp/praise/uranos2017/181221/" target="_blank"><img src="/2016/images/top/1605uranosuconcert.jpg"></a>
     <p class="block-date">日程：2018年12月21日</p>
     <p class="block-title"><a href="http://antiochblog.jp/praise/uranos2017/181221/" target="_blank">ウラノス<br>あきる野市の授産施設のクリスマス会でミニコンサート</a></p>
@@ -267,7 +266,6 @@
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
-    <span>NEW</span>
     <a href="http://astone.tv/events/gate2018/181221/" target="_blank"><img src="/2016/images/top/1606gateofnerima.jpg"></a>
     <p class="block-date">日程：2018年12月21日</p>
     <p class="block-title"><a href="http://astone.tv/events/gate2018/181221/" target="_blank">The Gate of Praise <br>川崎の老健でコンサート</a></p>
