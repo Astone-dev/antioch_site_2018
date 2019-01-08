8ccf5e86137bf1713f889671abde1354b1335934
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-01-08 23:03:13 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -39,6 +39,14 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
+    <a href="http://astone.tv/events/kashiwa1901/" target="_blank"><img src="/2016/images/top/1605kashiwamachida.jpg"></a>
+    <p class="block-date">日程：2019年1月9日</p>
+    <p class="block-title"><a href="http://astone.tv/events/kashiwa1901/" target="_blank">冬晴れの柏を行く<br>柏聖会</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
     <a href="http://antiochblog.jp/praise/thanks1901/" target="_blank"><img src="/2016/images/top/1801orinpikuantioch.jpg"></a>
     <p class="block-date">日程：2019年1月7日～13日</p>
     <p class="block-title"><a href="http://antiochblog.jp/praise/thanks1901/" target="_blank">冬休み明け さあ頑張るぞ<br>東京アンテオケ教会の一週間</a></p>
