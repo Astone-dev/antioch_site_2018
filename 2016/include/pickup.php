db64752fca7571e48570dee0951142882986225b
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-02-04 20:23:24 +0900

    pick up修正 カリスマ聖会修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -60,13 +60,13 @@
     <p class="block-title"><a href="http://antiochblog.jp/praise/faith1902/" target="_blank">それぞれの長崎殉教記念聖会 <br>東京アンテオケ教会</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/prayer1902/" target="_blank"><img src="/2016/images/top/1702inorinotour.jpg"></a>
-    <p class="block-date">日程：2019年2月4日～10日</p>
+    <p class="block-date">日程：2019年2月6日～</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/prayer1902/" target="_blank">26聖人の足跡をたどる<br> 祈りのツアー</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
