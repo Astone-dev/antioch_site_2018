471a62bbe77649c01c08898c952b8a0b5e621206
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-02-05 22:42:20 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -37,14 +37,6 @@
 </div>
 
 <div class="column-inner">
-<span class="label-2">映像　</span>
-<span>NEW</span>
-<a href="https://www.youtube.com/user/kaminoshimobe/live" target="_blank" onclick="javascript:pageTracker._trackPageview('日本二十六聖人を記念する集会');"><img src="/2016/images/top/1610hanbokushilive.png"></a>
-<p class="block-date">2019年2月5日 </p>
-<p class="block-title"><a href="https://www.youtube.com/user/kaminoshimobe/live" target="_blank" onclick="javascript:pageTracker._trackPageview('日本二十六聖人を記念する集会');">長崎殉教記念聖会 生中継<br>昼 14:00～／夜 19:00～</a><br><i style="color:#ff0000;font-size: 11px;">※Youtubeアプリが無いと視聴に問題が起こる可能性があります。Youtubeアプリをダウンロードしてご視聴ください。</i></p>
-</div>
-
-<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/yorokobi1902/" target="_blank"><img src="/2016/images/top/1607kyusyuseika.jpg"></a>
