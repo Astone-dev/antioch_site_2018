6ea514192f70db2e962269ae58d8ab18ad2fadee
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-02-16 17:39:34 +0900

    pick up変更

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -84,13 +84,13 @@
     <p class="block-title"><a href="http://astone.tv/events/treeoflife/190216/" target="_blank">Tree of Life<br>ららん藤岡でゴスペルコンサート</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/jerusalem2017/190216/" target="_blank"><img src="/2016/images/top/1606jerumisatode.jpg"></a>
     <p class="block-date">日程：2019年2月16日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190216/" target="_blank">エルサレム<br>さいたま市岩槻リハビリセンターでコンサート</a></p>
-</div>
+</div>-->
 
 <!--<div class="column-inner">
     <span class="label-1">ブログ</span>
