87cf9df417415a79ca05f6b630fff8c22b786f60
timo77 <timo77@antioch.jp>
2019-02-17 11:10:25 +0900

    pickup : U21・KIDS アンテオケ教会予選-生中継追加

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -52,6 +52,13 @@
     <p class="block-title"><a href="http://antiochblog.jp/info/yosen1902/" target="_blank">J.GOSPELコンテスト in 白馬<br>U21・KIDS アンテオケ教会予選に向かって<br>ドキドキの一週間</a></p>
 </div>
 
+<span class="label-2">映像</span>
+<span>NEW</span>
+<a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('U21・KIDS アンテオケ教会予選-生中継'); "><img src="/2016/images/top/1610revivalnightlive.png"></a>
+<p class="block-date">2019年2月17日 17:30～</p>
+<p class="block-title"><a href="https://www.youtube.com/channel/UCOV9zn7vX4O98uRzWlODMew/live" target="_blank" onclick="javascript:pageTracker._trackPageview('U21・KIDS アンテオケ教会予選-生中継');">U21・KIDS アンテオケ教会予選 生中継<br>(日程：2019年2月17日 17:30スタート)</a></p>
+</div>
+
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
