ccaacada495a82f0fd742b12da85b710cb204955
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2019-02-18 14:49:11 +0900

    pickup追加

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -64,8 +64,16 @@
     <span class="label-2">動画</span>
     <span>NEW</span>
     <a href="http://tv.antioch.jp/digest/201902-hollywood/" target="_blank"><img src="/2016/images/top/1902-hollywood-dst.jpg"></a>
-	<p class="block-date">2019年2月6日～2月13日</p>	
+	<p class="block-date">2019年2月6日～2月13日</p>
     <p class="block-title"><a href="http://tv.antioch.jp/digest/201902-hollywood/" target="_blank">ハリウッドチーム<br>ダイジェスト映像</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-2">動画</span>
+    <span>NEW</span>
+    <a href="https://tokyo.antioch.jp/internet-tv/190216-antiochyosen.html" target="_blank"><img src="/2016/images/top/1902hakuu21antiochyosen_m.jpg"></a>
+    <p class="block-date">日程：2019年2月17日</p>
+    <p class="block-title"><a href="https://tokyo.antioch.jp/internet-tv/190216-antiochyosen.html" target="_blank">J.GOSPELコンテスト in 白馬<br>U21・KIDS東京アンテオケ教会予選<br>映像保存版</a></p>
 </div>
 
 <div class="column-inner">
@@ -73,7 +81,7 @@
     <span>NEW</span>
     <a href="http://astone.tv/events/hakuba1902z/" target="_blank"><img src="/2016/images/top/1802sunerugoinimukatezenkoku.jpg"></a>
     <p class="block-date">ブログ：2019年2月2月18日～24日</p>
-    <p class="block-title"><a href="http://astone.tv/events/hakuba1902z/" target="_blank">いよいよ来週 白馬スネルゴイキャンプに向かって <br>全国各教会わくわくの一週間 </a></p>
+    <p class="block-title"><a href="http://astone.tv/events/hakuba1902z/" target="_blank">いよいよ来週 白馬スネルゴイキャンプに向かって <br>全国各教会わくわくの一週間</a></p>
 </div>
 
 <div class="column-inner">
