aa7a9768ffa02b5e62dc593c8cafd2fda443e8ca
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-03-06 23:28:19 +0900

    pick up修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -79,17 +79,17 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/info/jerusalem2017/190307/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+    <a href="http://antiochblog.jp/info/march1903z/" target="_blank"><img src="/2016/images/top/1702harchikaizenkou.jpg"></a>
     <p class="block-date">日程：2019年3月4日～10日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190307/" target="_blank">エルサレム<br>箱根のホテルでロビーコンサート</a></p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/march1903z/" target="_blank">春一番が吹く季節<br>TLEA全国各教会メンバーの一週間</a></p>
 </div>
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/info/march1903z/" target="_blank"><img src="/2016/images/top/1702harchikaizenkou.jpg"></a>
+    <a href="http://antiochblog.jp/info/jerusalem2017/190307/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
     <p class="block-date">日程：2019年3月7日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/march1903z/" target="_blank">春一番が吹く季節<br>TLEA全国各教会メンバーの一週間</a></p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190307/" target="_blank">エルサレム<br> 箱根のホテルでロビーコンサート</a></p>
 </div>
 
 <div class="column-inner">
