ce3270c29b916e3485fa2b50bf84935a7dc68cac
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-03-11 23:00:22 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -69,6 +69,14 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/info/jerusalem2017/190312/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
+    <p class="block-date">日程：2019年3月12日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190312/" target="_blank">エルサレム<br>三郷のアカシアの家オレンジカフェでコンサート</a></p>
+</div>
+
+<div class="column-inner">
     <span class="label-2">動画</span>
     <span>NEW</span>
     <a href="http://atv.antioch.jp/hcff2019/" target="_blank"><img src="/2016/images/top/1902hcffnyuusyou_m.jpg"></a>
@@ -238,12 +246,12 @@
 <p class="block-title"><a href="http://astone.tv/events/2017healing/190309/" target="_blank">3月 いやしの集いの一日</a></p>
 </div>
 
-<div class="column-inner">
+<<!--div class="column-inner">
     <span class="label-1">ブログ</span>
     <a href="http://antiochblog.jp/info/jerusalem2017/190309/" target="_blank"><img src="/2016/images/top/1706jerumisato.jpg"></a>
     <p class="block-date">日程：2019年3月9日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190309/" target="_blank">エルサレム<br> さいたま市岩槻リハビリセンターコンサート</a></p>
-</div>
+</div>-->
 
 <!--<div class="column-inner">
     <span class="label-1">ブログ</span>
