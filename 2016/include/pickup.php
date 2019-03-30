2c13175abb54af246fbd60fac53e88f235737846
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-03-30 10:32:00 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -38,6 +38,14 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/info/gifts/190330/" target="_blank"><img src="/2016/images/top/1703giftsshaonkai.jpg"></a>
+    <p class="block-date">日程：2019年3月30日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/gifts/190330/" target="_blank">Gifts<br>練馬の施設でゴスペルコンサート</a></p>
+</div>
+
+<div class="column-inner">
     <span class="label-2">動画</span>
     <span>NEW</span>
     <a href="http://tv.antioch.jp/digest/201803-merline/" target="_blank"><img src="/2016/images/top/1903merlin_m.jpg"></a>
