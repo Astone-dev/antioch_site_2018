917b94b946f2a0acca14f4df5b0ef2b2114db054
timo77 <timo77@antioch.jp>
2019-03-31 09:53:51 +0900

    pickup: OH! HAPPY DAY! ブログ追加

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -55,6 +55,14 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
+    <a href="http://astone.tv/events/happyday2017/201903/" target="_blank"><img src="/2016/images/top/1701ohhappyday.jpg"></a>
+    <p class="block-date">日程：2019年3月31日</p>
+    <p class="block-title"><a href="http://astone.tv/events/happyday2017/201903/" target="_blank">OH! HAPPY DAY!の一日</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
     <a href="https://tokyo.antioch.jp/tcm/" target="_blank"><img src="/2016/images/top/1903tocharidigest_p.jpg"></a>
     <p class="block-date">日程：2019年3月6日</p>
     <p class="block-title"><a href="https://tokyo.antioch.jp/tcm/" target="_blank">3月の東京カリスマ聖会<br>ダイジェスト写真集<br>(きれいな写真版)</a></p>
