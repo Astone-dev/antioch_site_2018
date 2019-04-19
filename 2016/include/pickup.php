1ec1288dd36c7645cba0b5a78d3a6fce2f895200
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-04-19 22:58:43 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -38,6 +38,22 @@
 </div>
 
 <div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/praise/light1904/" target="_blank"><img src="/2016/images/top/1703giftsshaonkai.jpg"></a>
+    <p class="block-date">日程：2019年4月20日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/light1904/" target="_blank">Gifts漫遊記　<br>春の信州上田を行く </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://antiochblog.jp/info/revival2017/190420/" target="_blank"><img src="/2016/images/top/1703revivalnight.jpg"></a>
+    <p class="block-date">日程：2019年4月20日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/revival2017/190420/" target="_blank">4月 リバイバルナイトの1日 </a></p>
+</div>
+
+<div class="column-inner">
     <span>NEW</span>
     <a href="https://tokyo.antioch.jp/hakuba/" target="_blank"><img src="/2016/images/top/1904hakubacamp.jpg"></a>
 	<p class="block-date">キャンプ：2019年7月23日〜26日</p>	
@@ -72,6 +88,22 @@
     <a href="http://astone.tv/events/light1904z/" target="_blank"><img src="/2016/images/top/1704shingakkizenkoku.jpg"></a>
     <p class="block-date">日程：4月15日～21日</p>
     <p class="block-title"><a href="http://astone.tv/events/light1904z/" target="_blank">緑の風爽やかに<br> TLEA全国各教会メンバーの一週間</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>    
+    <a href="http://antiochblog.jp/info/jerusalem2017/190420/" target="_blank"><img src="/2016/images/top/1606jeruhakone.jpg"></a>
+    <p class="block-date">日程：2019年4月13日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190420/" target="_blank">エルサレム<br>岩槻デイサービスでコンサート </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>          
+    <a href="http://antiochblog.jp/praise/gracejewel2017/190420/" target="_blank"><img src="/2016/images/top/1901gracehosekikoenji.jpg"></a>
+    <p class="block-date">日程：2019年4月20日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/190420/" target="_blank">GRACEと宝石<br>埼玉県志木市の特別養護老人ホームでゴスペルコンサート</a></p>
 </div>
 
 <div class="column-inner">
@@ -178,8 +210,6 @@
     <p class="block-title"><a href="http://atv.antioch.jp/kazenohibiki" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え15組のパーソナリティー</a></p>
 </div>
 
-
-
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <a href="http://antiochblog.jp/praise/kyusyu1904/" target="_blank"><img src="/2016/images/top/1610kyusyuseika.jpg"></a>
@@ -250,12 +280,12 @@
     <p class="block-title"><a href="http://antiochblog.jp/praise/patolis/190413/" target="_blank">PATOLIS<br>練馬区のデイサービスでゴスペルコンサート</a></p>
 </div>-->
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <a href="http://antiochblog.jp/info/jerusalem2017/190413/" target="_blank"><img src="/2016/images/top/1606jeruhakone.jpg"></a>
     <p class="block-date">日程：2019年4月13日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190413/" target="_blank">エルサレム<br>三郷市文化会館にてチャリティーコンサート </a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
@@ -264,12 +294,12 @@
     <p class="block-title"><a href="http://antiochblog.jp/info/gracious/190413/" target="_blank">GRACIOUS DOORS <br>都内のデイサービスで、ボランティア・コンサート</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>      
     <a href="http://antiochblog.jp/praise/gracejewel2017/190412/" target="_blank"><img src="/2016/images/top/1901gracehosekikoenji.jpg"></a>
     <p class="block-date">日程：2019年4月12日</p>
     <p class="block-title"><a href="http://antiochblog.jp/praise/gracejewel2017/190412/" target="_blank">GRACEと宝石<br>高円寺にあるデイサービスでゴスペルコンサート</a></p>
-</div>
+</div>-->
 
 <!--<div class="column-inner">
     <span class="label-2">動画</span>
