9b05e43f82526f149316d9ef3a822c77ac8f429f
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-04-23 23:18:36 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -63,6 +63,14 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
+    <a href="http://antiochblog.jp/info/concert1904/" target="_blank"><img src="/2016/images/top/1710immberacro.jpg"></a>
+    <p class="block-date">日程：2019年4月24日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/concert1904/" target="_blank">Immanuel、Beracha、CROIX、 <br>杉並区の特別養護老人施設でコンサート </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
     <a href="http://astone.tv/events/gw1904z/" target="_blank"><img src="/2016/images/top/1704goldenweedmaezenkoku.jpg"></a>
     <p class="block-date">日程：4月22日～28日</p>
     <p class="block-title"><a href="http://astone.tv/events/gw1904z/" target="_blank">ゴールデンウイーク直前 <br> TLEA全国各教会メンバーワクワクの一週間</a></p>
