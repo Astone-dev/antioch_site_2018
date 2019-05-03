4552044514ab26c754cd0fb5534e8a5e7b781251
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-05-03 23:20:42 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -39,9 +39,17 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/info/1905meetings/" target="_blank"><img src="/2016/images/top/1605kashiwamachida.jpg"></a>
-    <p class="block-date">日程：2019年5月3日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/1905meetings/" target="_blank">大型連休真っ只中の柏を行く<br>関東宣教の旅 </a></p>
+    <a href="http://astone.tv/events/1905machida/" target="_blank"><img src="/2016/images/top/1701machida.jpg"></a>
+    <p class="block-date">日程：2019年5月4日</p>
+    <p class="block-title"><a href="http://astone.tv/events/1905machida/" target="_blank">青葉の季節の町田を行く<br>関東宣教の旅 </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://astone.tv/events/tocari1905/" target="_blank"><img src="/2016/images/top/1703tokyocharisma.jpg"></a>
+    <p class="block-date">日程：2019年5月4日～5月5日</p>
+    <p class="block-title"><a href="http://astone.tv/events/tocari1905/" target="_blank">風薫る<br>5月の東京カリスマ聖会スタッフ大忙し </a></p>
 </div>
 
 <div class="column-inner">
@@ -66,14 +74,6 @@
     <a href="http://astone.tv/events/gw1905z/" target="_blank"><img src="/2016/images/top/1805gwzenkoku.jpg"></a>
     <p class="block-date">日程：2019年4月29日～5月5日</p>
     <p class="block-title"><a href="http://astone.tv/events/gw1905z/" target="_blank">TLEA全国各教会メンバー<br>大型ゴールデンウイーク一週間 </a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/info/gracious/190503/" target="_blank"><img src="/2016/images/top/1605graciousborantia.jpg"></a>
-    <p class="block-date">日程：2019年5月3日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/gracious/190503/" target="_blank">GRACIOUS DOORS <br>都内のデイサービスで、ボランティア・コンサート </a></p>
 </div>
 
 <div class="column-inner">
@@ -180,6 +180,21 @@
     <p class="block-date">毎日19:00更新</p>
     <p class="block-title"><a href="http://atv.antioch.jp/kazenohibiki" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え15組のパーソナリティー</a></p>
 </div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/info/1905meetings/" target="_blank"><img src="/2016/images/top/1605kashiwamachida.jpg"></a>
+    <p class="block-date">日程：2019年5月3日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/1905meetings/" target="_blank">大型連休真っ只中の柏を行く<br>関東宣教の旅 </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/info/gracious/190503/" target="_blank"><img src="/2016/images/top/1605graciousborantia.jpg"></a>
+    <p class="block-date">日程：2019年5月3日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/gracious/190503/" target="_blank">GRACIOUS DOORS <br>都内のデイサービスで、ボランティア・コンサート </a></p>
+</div>
+
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
