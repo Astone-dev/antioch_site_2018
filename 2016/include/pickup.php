fc91483d77fbf675652da3d297144ca7dab55c94
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-05-10 23:36:01 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -39,9 +39,9 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://astone.tv/events/1905yokohama/" target="_blank"><img src="/2016/images/top/1702yokohamaseikai.jpg"></a>
-    <p class="block-date">日程：2019年5月10日</p>
-    <p class="block-title"><a href="http://astone.tv/events/1905yokohama/" target="_blank">急に初夏の関東を行く<br>横浜聖会 </a></p>
+    <a href="http://antiochblog.jp/info/1905meeting/" target="_blank"><img src="/2016/images/top/1808higashitokyoseikai.jpg"></a>
+    <p class="block-date">日程：2019年5月11日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/1905meeting/" target="_blank">初夏の訪れを感じながら <br>東東京聖会 </a></p>
 </div>
 
 <div class="column-inner">
@@ -50,6 +50,14 @@
     <a href="http://antiochblog.jp/info/thanks1905/" target="_blank"><img src="/2016/images/top/1807hakubaanntioch.jpg"></a>
     <p class="block-date">日程：2019年5月6日～5月12日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/thanks1905/" target="_blank">白馬キャンプ申し込みスタート <br>東京アンテオケ教会メンバーワクワクのー週間 </a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://astone.tv/events/2017healing/190511/" target="_blank"><img src="/2016/images/top/1706iyashinotsudoi.jpg"></a>
+    <p class="block-date">日程：2019年5月11日</p>
+    <p class="block-title"><a href="http://astone.tv/events/2017healing/190511/" target="_blank">4月のいやしの集いの一日 </a></p>
 </div>
 
 <div class="column-inner">
@@ -171,6 +179,13 @@
     <a href="http://atv.antioch.jp/kazenohibiki" target="_blank"><img src="/2016/images/top/1604kazenohibikiradio.jpg"></a>
     <p class="block-date">毎日19:00更新</p>
     <p class="block-title"><a href="http://atv.antioch.jp/kazenohibiki" target="_blank">インターネットラジオ<br>ゴスペル風のひびき<br>心に触れるゴスペルと楽しいおしゃべり 取っ替え引っ替え15組のパーソナリティー</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://astone.tv/events/1905yokohama/" target="_blank"><img src="/2016/images/top/1702yokohamaseikai.jpg"></a>
+    <p class="block-date">日程：2019年5月10日</p>
+    <p class="block-title"><a href="http://astone.tv/events/1905yokohama/" target="_blank">急に初夏の関東を行く<br>横浜聖会 </a></p>
 </div>
 
 <div class="column-inner">
