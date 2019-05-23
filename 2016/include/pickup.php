ced66425a47ffa0fcddb00443836a8f1040b837e
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-05-24 00:34:59 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -40,6 +40,14 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
+    <a href="http://antiochblog.jp/info/gifts/190524/" target="_blank"><img src="/2016/images/top/1905gifts.jpg"></a>
+    <p class="block-date">日程：2019年5月24日～25日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/gifts/190524/" target="_blank">Gifts <br>箱根のホテルでゴスペルコンサート</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
     <a href="http://astone.tv/events/faith1905z/" target="_blank"><img src="/2016/images/top/1707summerz.jpg"></a>
     <p class="block-date">日程：2019年5月20日～5月26日</p>
     <p class="block-title"><a href="http://astone.tv/events/faith1905z/" target="_blank">初夏の風が清々しい<br>TLEA全国各教会メンバーの一週間</a></p>
