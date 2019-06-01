901a6276a32e21837647a974214ae35004ef344c
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2019-06-01 09:32:42 +0900

    pickup修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -53,13 +53,13 @@
     <p class="block-title"><a href="http://antiochblog.jp/praise/grace1905/" target="_blank">初夏の香りに包まれて<br>東京アンテオケ教会メンバーほのぼの一週間</a></p>
 </div>
 
-<div class="column-inner">
+<!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/gracious/190601/" target="_blank"><img src="/2016/images/top/1610gracioustonai.jpg"></a>
     <p class="block-date">日程：2019年6月1日</p>
     <p class="block-title"><a href="http://antiochblog.jp/info/gracious/190601/" target="_blank">GRACIOUS DOORS <br>都内のデイサービスで、ボランティア・コンサート</a></p>
-</div>
+</div>-->
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
