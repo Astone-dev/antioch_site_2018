0ec45eac7538181799837c291f2c1eec3b8cd473
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-06-02 22:20:35 +0900

    カリスマ聖会更新

--- 2016/include/next_charisma.php
+++ 2016/include/next_charisma.php
@@ -3,10 +3,10 @@
 // ""の間のみ編集可能。他の箇所を触れるとWEBが壊れるため注意！ 
 // 例1: $next_charisma_date = "2015年7月7日(日)";
 // 例2: $next_charisma_date = "2017年12月11日(日)";
-$next_charisma_date = " 2019年6月2日(日) ";
+$next_charisma_date = " 2019年7月7日(日) ";
 
 
 // 英語版 のカリスマ聖会の年月日を記載
 // 例: $next_charisma_date_eng = " Aug 21st, Sun.";
-$next_charisma_date_eng = "Jun.2nd,Sun.";
+$next_charisma_date_eng = "Jul.7th,Sun.";
 ?>
