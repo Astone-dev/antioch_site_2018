085249651293038092892dcfbfe137d602c5437b
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-06-10 07:13:17 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -37,7 +37,7 @@
     <p class="block-title"><a href="http://antiochblog.jp/praise/kansai1906/" target="_blank">夏近づく関西を行く <br>関西宣教の旅</a></p>
 </div>
 
-<!--<div class="column-inner">
+<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://antiochblog.jp/praise/faith1906/" target="_blank"><img src="/2016/images/top/1705tsuyumaeantioch.jpg"></a>
@@ -50,7 +50,7 @@
     <span>NEW</span>
     <a href="http://antiochblog.jp/info/hibiki1906/" target="_blank"><img src="/2016/images/top/1706sakushiyorokobizenkoku.jpg"></a>
     <p class="block-date">日程：2019年6月10日～16日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/hibiki1906/" target="_blank">風のひびき第7集募集開始 <br>作詞作曲者たちの驚くべき野心</a></p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/hibiki1906/" target="_blank">風のひびき第7集募集開始  <br>作詞作曲者たちの驚くべき野心</a></p>
 </div>
 
 <div class="column-inner">
@@ -59,22 +59,6 @@
     <a href="http://astone.tv/events/faith1906z/" target="_blank"><img src="/2016/images/top/1705tsuyumaezenkoku.jpg"></a>
     <p class="block-date">日程：2019年6月10日～16日</p>
     <p class="block-title"><a href="http://astone.tv/events/faith1906z/" target="_blank">紫陽花の季節 <br>TLEA全国各教会メンバーの一週間</a></p>
-</div>-->
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/june1906/" target="_blank"><img src="/2016/images/top/1707hakubajyunbi.jpg"></a>
-    <p class="block-date">日程：2019年6月3日～9日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/june1906/" target="_blank">白馬キャンプに向かって<br>東京アンテオケ教会メンバー ワクワクな一週間<br>（思い出を振り返りつつ）</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
-    <a href="http://astone.tv/events/june1906z/" target="_blank"><img src="/2016/images/top/1707hakubachokuzenzenkoku.jpg"></a>
-    <p class="block-date">日程：2019年6月3日～9日</p>
-    <p class="block-title"><a href="http://astone.tv/events/june1906z/" target="_blank">白馬キャンプに向かって<br>全国各教会 ワクワクな一週間<br>（思い出を振り返りつつ）</a></p>
 </div>
 
 <div class="column-inner">
@@ -194,6 +178,21 @@
     <p class="block-date">キャンプ：2019年7月23日〜26日</p>
     <p class="block-title"><a href="http://tv.antioch.jp/digest/201905-hakubacm/" target="_blank">白馬キャンプ<br>紹介映像 Part2</a></p>
 </div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/june1906/" target="_blank"><img src="/2016/images/top/1707hakubajyunbi.jpg"></a>
+    <p class="block-date">日程：2019年6月3日～9日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/june1906/" target="_blank">白馬キャンプに向かって<br>東京アンテオケ教会メンバー ワクワクな一週間<br>（思い出を振り返りつつ）</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://astone.tv/events/june1906z/" target="_blank"><img src="/2016/images/top/1707hakubachokuzenzenkoku.jpg"></a>
+    <p class="block-date">日程：2019年6月3日～9日</p>
+    <p class="block-title"><a href="http://astone.tv/events/june1906z/" target="_blank">白馬キャンプに向かって<br>全国各教会 ワクワクな一週間<br>（思い出を振り返りつつ）</a></p>
+</div>
+
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
