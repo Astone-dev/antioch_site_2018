07a46527d8c87c742400419bbf9b3885e3998a1f
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-07-21 18:22:34 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -39,9 +39,17 @@
 <!--<div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/give1907/" target="_blank"><img src="/2016/images/top/1804semi_a.jpg"></a>
-    <p class="block-date">日程：2019年7月8日～14日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/give1907/" target="_blank">梅雨空を吹きとばせ！<br>東京アンテオケ教会メンバーの一週間　</a></p>
+    <a href="http://antiochblog.jp/praise/eternal1907/" target="_blank"><img src="/2016/images/top/1804semi_a.jpg"></a>
+    <p class="block-date">日程：2019年7月22日～28日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/eternal1907/" target="_blank">   <br>東京アンテオケ教会メンバーの一週間　</a></p>
+</div>-->
+
+<!--<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <span>NEW</span>
+    <a href="http://astone.tv/events/eternal1907z/" target="_blank"><img src="/2016/images/top/1804semi_a.jpg"></a>
+    <p class="block-date">日程：2019年7月22日～28日</p>
+    <p class="block-title"><a href="http://astone.tv/events/eternal1907z/" target="_blank">   <br>東京アンテオケ教会メンバーの一週間　</a></p>
 </div>-->
 
 <div class="column-inner">
