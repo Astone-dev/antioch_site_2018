c6718944698bef71c7d09939f78ff37980a8b52e
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-08-11 16:49:42 +0900

    pick up更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -95,7 +95,7 @@
     <span class="label-1">ブログ</span>
     <span>NEW</span>
     <a href="http://astone.tv/events/summer1908z/" target="_blank"><img src="/2016/images/top/1806chichinohizenkoku.jpg"></a>
-    <p class="block-date">日程：2019年8月5日～11日</p>
+    <p class="block-date">日程：2019年8月12日～18日</p>
     <p class="block-title"><a href="http://astone.tv/events/summer1908z/" target="_blank">夏休み本番<br>TLEA全国各教会メンバーの一週間</a></p>
 </div>-->
 
