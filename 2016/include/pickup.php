fde0b187e4e53cda6521d4546ac68b23e5aee6ce
DESKTOP-LKEUCE6\johnf <johnfuji4845@gmail.com>
2019-09-07 23:13:18 +0900

    pick up 更新

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -64,14 +64,6 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/info/jerusalem2017/190907/" target="_blank"><img src="/2016/images/top/1704jerusaitama.jpg"></a>
-    <p class="block-date">日程：2019年9月7日</p>
-    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190907/" target="_blank">エルサレム<br>埼玉県岩槻西グループホームでゴスペルコンサート</a></p>
-</div>
-
-<div class="column-inner">
-    <span class="label-1">ブログ</span>
-    <span>NEW</span>
     <a href="http://astone.tv/events/start1909z/" target="_blank"><img src="/2016/images/top/1709shingakkizenkoku.jpg"></a>
     <p class="block-date">日程：2019年9月2日～8日</p>
     <p class="block-title"><a href="http://astone.tv/events/start1909z/" target="_blank">始まった新学期 <br>TLEA全国各教会の一週間</a></p>
@@ -197,6 +189,13 @@
     <a href="http://tv.antioch.jp/digest/201905-kobayashi/" target="_blank"><img src="/2016/images/top/1905hanna_m.jpg"></a>
     <p class="block-title"><a href="http://tv.antioch.jp/digest/201905-kobayashi/" target="_blank">小林ハンナさん<br>人気TV番組に出演映像<br>※著作権の関係により、出演シーンのみの配信となります</a></p>
 </div>-->
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/info/jerusalem2017/190907/" target="_blank"><img src="/2016/images/top/1704jerusaitama.jpg"></a>
+    <p class="block-date">日程：2019年9月7日</p>
+    <p class="block-title"><a href="http://antiochblog.jp/info/jerusalem2017/190907/" target="_blank">エルサレム<br>埼玉県岩槻西グループホームでゴスペルコンサート</a></p>
+</div>
 
 <div class="column-inner">
     <span class="label-1">ブログ</span>
