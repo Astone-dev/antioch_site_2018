ba55274f556b018e34c0825def9e1e99a1c61db0
Samuel N Kasahara <samuel.livebyfaith@gmail.com>
2019-09-12 11:42:05 +0900

    PICKUP修正

--- 2016/include/pickup.php
+++ 2016/include/pickup.php
@@ -32,9 +32,9 @@
 <div class="column-inner">
     <span class="label-1">ブログ</span>
     <span>NEW</span>
-    <a href="http://antiochblog.jp/praise/nagasaki1909b/" target="_blank"><img src="/2016/images/top/1907olympic_antioch.jpg"></a>
-    <p class="block-date">日程：2019年8月30日～</p>
-    <p class="block-title"><a href="http://antiochblog.jp/praise/nagasaki1909b/" target="_blank">いよいよ今週出発<br>第2長崎チーム<br>直前ブログ</a></p>
+    <a href="http://astone.tv/events/1908nagasaki/" target="_blank"><img src="/2016/images/top/1908nagasaki.jpg"></a>
+    <p class="block-date">ブログ：2019年9月6日～</p>
+    <p class="block-title"><a href="http://astone.tv/events/1908nagasaki/" target="_blank">いよいよ今週出発<br>第2長崎チーム<br>直前ブログ</a></p>
 </div>
 
 <div class="column-inner">
@@ -195,6 +195,13 @@
     <span class="label-2">動画</span>
     <a href="https://www.youtube.com/watch?v=_LiPoa6YaqQ" target="_blank" onclick="javascript:pageTracker._trackPageview('【セミナー】悪霊はいるのか'); "><img src="/2016/images/top/1908seminar190804.jpg"></a>
     <p class="block-title"><a href="https://www.youtube.com/watch?v=_LiPoa6YaqQ" target="_blank" onclick="javascript:pageTracker._trackPageview('【セミナー】悪霊はいるのか');">【セミナー】<br>悪霊はいるのか<br>講師：パウロ秋元牧師</a></p>
+</div>
+
+<div class="column-inner">
+    <span class="label-1">ブログ</span>
+    <a href="http://antiochblog.jp/praise/nagasaki1909b/" target="_blank"><img src="/2016/images/top/1907olympic_antioch.jpg"></a>
+    <p class="block-date">日程：2019年8月30日～</p>
+    <p class="block-title"><a href="http://antiochblog.jp/praise/nagasaki1909b/" target="_blank">第1長崎チーム<br>ブログ</a></p>
 </div>
 
 <!--ここから固定-->
